2e4f54d7f824e736d8f511157e1cf6fbcc2e5f26
dabluck <1688770+dabluck@users.noreply.github.com>
2026-09-15 19:41:35 -0400

    some minor project cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,11 +68,7 @@ dependencies {
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.truth)
     testImplementation(libs.turbine)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.truth)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
